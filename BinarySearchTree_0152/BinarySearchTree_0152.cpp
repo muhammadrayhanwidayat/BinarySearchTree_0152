6312495a6c8b99ddba7ea751ef9049313f319b01
muhammadrayhanwidayat <murayat99@gmail.com>
2024-06-13 11:26:24 +0700

    membuat prosedur inorder

--- BinarySearchTree_0152/BinarySearchTree_0152.cpp
+++ BinarySearchTree_0152/BinarySearchTree_0152.cpp
@@ -77,6 +77,19 @@ public:
                 currentNode = currentNode->rightChild;
         }
     }
+    //membuat dan mendefinisikan prosedur inorder
+    void inorder(Node* ptr) {
+
+        if (ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr) {
+            inorder(ptr->leftChild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightChild);
+        }
+    }
 
 };
 
