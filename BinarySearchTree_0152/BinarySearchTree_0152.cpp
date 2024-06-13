8fd978edd0156ebbf403eb963eb79dce2b04e49f
muhammadrayhanwidayat <murayat99@gmail.com>
2024-06-13 11:18:32 +0700

    membuat looping

--- BinarySearchTree_0152/BinarySearchTree_0152.cpp
+++ BinarySearchTree_0152/BinarySearchTree_0152.cpp
@@ -70,9 +70,11 @@ public:
         parent = nullptr;
         while ((currentNode != nullptr) && (currentNode->info != element))
         {
-
-
-
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftChild;
+            else
+                currentNode = currentNode->rightChild;
         }
     }
 
